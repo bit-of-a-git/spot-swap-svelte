6f1bc973b9f1d7d6699d218b3cf376591351f6cf
David O' Connor <92552641+bit-of-a-git@users.noreply.github.com>
2025-04-16 20:28:40 +0100

    End of lab 19a

---
+++ src/routes/signup/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import SignupForm from './SignupForm.svelte';
+</script>
+
+<section class="hero is-fullheight is-default is-bold">
+	<div class="hero-head"></div>
+	<div class="hero-body">
+		<div class="container">
+			<div class="columns is-vcentered">
+				<div class="column is-5">
+					<h1 class="title has-text-centered">Sign up to SpotSwap</h1>
+					<SignupForm />
+				</div>
+				<div class="column is-5 is-offset-1">
+					<figure class="image">
+						<img class="object-fit" src="/globe.jpg" alt="globe" />
+					</figure>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
