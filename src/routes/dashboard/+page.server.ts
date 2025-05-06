e31e3dfb2df425f3b1a94bca38a877508f112304
David O' Connor <92552641+bit-of-a-git@users.noreply.github.com>
2025-05-06 13:03:29 +0100

    Adding server-side element to dashboard for adding collections, enhance function upon add collection success

---
+++ src/routes/dashboard/+page.server.ts
@@ -0,0 +1,33 @@
+import { spotswapService } from '$lib/services/spotswap-service';
+import type { Session } from '$lib/types/collection-types';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ cookies }) => {
+	const cookieStr = cookies.get('spotswap-user') as string;
+	if (cookieStr) {
+		const session = JSON.parse(cookieStr) as Session;
+		const userCollections = await spotswapService.getUserCollections(session._id, session.token);
+		console.log(userCollections);
+		return {
+			userCollections
+		};
+	}
+};
+
+export const actions = {
+	addCollection: async ({ request, cookies }) => {
+		const cookieStr = cookies.get('spotswap-user') as string;
+		if (cookieStr) {
+			const session = JSON.parse(cookieStr) as Session;
+			if (session) {
+				const form = await request.formData();
+				const collection = {
+					title: form.get('title') as string,
+					county: form.get('county') as string
+				};
+				const newCollection = await spotswapService.addCollection(collection, session.token);
+				return newCollection;
+			}
+		}
+	}
+};
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,10 +1,33 @@
 <script lang="ts">
-	import { subTitle } from '$lib/runes.svelte';
+	import { userCollections, subTitle } from '$lib/runes.svelte';
+	import type { ActionResult } from '@sveltejs/kit';
 	import Card from '$lib/ui/Card.svelte';
 	import CollectionForm from './CollectionForm.svelte';
 	import CollectionList from '$lib/ui/CollectionList.svelte';
+	import { onMount } from 'svelte';
+	import { refreshUserState } from '$lib/services/collection-utils';
+	import type { Collection } from '$lib/types/collection-types';
+	import type { PageProps } from './$types';
 
 	subTitle.text = 'Dashboard';
+
+	let { data }: PageProps = $props();
+	let message = $state('');
+
+	const handleCollectionSuccess = () => {
+		return async ({ result }: { result: ActionResult }) => {
+			if (result.type === 'success') {
+				const collection = result.data as Collection;
+				userCollections.collections.push(collection);
+				refreshUserState(userCollections.collections);
+				message = `You added a "${collection.title}" collection to Co. ${collection.county}`;
+			}
+		};
+	};
+
+	onMount(async () => {
+		await refreshUserState(data.userCollections);
+	});
 </script>
 
 <div class="columns">
@@ -15,7 +38,7 @@
 	</div>
 	<div class="column">
 		<Card title="Add a Collection">
-			<CollectionForm />
+			<CollectionForm enhanceFn={handleCollectionSuccess} {message} />
 		</Card>
 	</div>
 </div>
